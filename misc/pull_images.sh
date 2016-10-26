74eba00226c478d0f09867cb18bbb84207a472fe
Valentin Ouvrard <valentin.ouvrard@nautile.sarl>
2016-10-27 10:03:36 +1100

    migrate to Drupal 8.2.1

--- misc/pull_images.sh
+++ misc/pull_images.sh
@@ -5,7 +5,7 @@ wget https://wordpress.org/latest.tar.gz -O /tmp/wordpress.tar.gz
 #wget https://github.com/joomla/joomla-cms/releases/download/3.6.2/Joomla_3.6.2-Stable-Full_Package.zip -O /tmp/joomla.zip
 
 ## Drupal
-wget https://ftp.drupal.org/files/projects/drupal-8.1.8.tar.gz -O /tmp/drupal.tar.gz
+wget https://ftp.drupal.org/files/projects/drupal-8.2.1.tar.gz -O /tmp/drupal.tar.gz
 
 ## Docker
 docker pull php:7.0.9-fpm

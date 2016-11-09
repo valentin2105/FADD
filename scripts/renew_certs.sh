967d9d31b70aa908a1c9b3d796d6ee8888a2b295
ops-srv01 <srv01@ouvrard.it>
2016-11-09 22:12:53 +0000

    add acmePath to renewCerts

--- scripts/renew_certs.sh
+++ scripts/renew_certs.sh
@@ -1,1 +1,2 @@
-docker run -it --rm --name letsencrypt -v "/etc/letsencrypt:/etc/letsencrypt" -v "/srv/letsencrypt.sh/challenge:/srv/letsencrypt.sh/challenge" -v "/var/lib/letsencrypt:/var/lib/letsencrypt" quay.io/letsencrypt/letsencrypt renew -n --webroot --webroot-path /srv/letsencrypt.sh/challenge  -m contact@example.com
+acmePath=/srv/acme/challenges
+docker run -it --rm --name letsencrypt -v "/etc/letsencrypt:/etc/letsencrypt" -v "$acmePath:$acmePath" -v "/var/lib/letsencrypt:/var/lib/letsencrypt" quay.io/letsencrypt/letsencrypt renew -n --webroot --webroot-path $acmePath  -m expiry@ouvrard.it
